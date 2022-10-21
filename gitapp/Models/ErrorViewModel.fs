5986fd1f51a1eb109355a952f3bc06c4591f3b28
Vaibgav <tarunjainvks@gmail.com>
2022-10-21 20:01:19 +0530

    Add project files.

---
+++ gitapp/Models/ErrorViewModel.fs
@@ -0,0 +1,8 @@
+namespace gitapp
+
+open System
+
+type ErrorViewModel private () =
+    member val RequestId : string = null with get, set
+
+    member val ShowRequestId : bool = true with get, set
